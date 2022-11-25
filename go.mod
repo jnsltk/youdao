113559c0f6c82099b0dcfcadaa0baf4557255c9a
jnsltk <janos.litkei@gmail.com>
2022-11-25 12:13:15 +0100

    Add colours, including pinyin tones colours

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/jnsltk/youdao
 
 go 1.19
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/mattn/go-colorable v0.1.9
+	github.com/mattn/go-isatty v0.0.14
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
+)
--- main.go
+++ main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/fatih/color"
 )
 
+// Perhaps in the future should change to web scraping as the api is deprecated and might become unavailable in the future
 const apiUrl = "https://fanyi.youdao.com/openapi.do?keyfrom=blog125&key=21376174&type=data&doctype=json&version=1.1&q="
+var tones = [][]string {
+	{"ā", "ē", "ī", "ō", "ū", "ǖ"},
+	{"á", "é", "í", "ó", "ú", "ǘ"},
+	{"ǎ", "ě", "ǐ", "ǒ", "ǔ", "ǚ"},
+	{"à", "è", "ì", "ò", "ù", "ǜ"},
+	{"a", "e", "i", "o", "u", "ü"},
+}
 
 func main() {
 	var word string
 	if len(os.Args) != 2 {
-		fmt.Print("Usage: youdao <Word> ")
+		fmt.Print("Usage: youdao <Word>")
+		os.Exit(1)
 	} else {
 		word = os.Args[1]
 	}
@@ -34,7 +46,7 @@ func getEntry(word string) *Entry {
 	var entry = new(Entry)
 
 	resp, err := http.Get(getUrl(word))
-	cont, err := ioutil.ReadAll(resp.Body)
+	cont, err := io.ReadAll(resp.Body)
 	err = json.Unmarshal([]byte(string(cont)), &entry)
 
 	if err != nil {
@@ -45,37 +57,111 @@ func getEntry(word string) *Entry {
 }
 
 func printEntry(entry *Entry) {
+	// Check if there's a result
+	if len(entry.Basic.Explains) == 0 {
+		fmt.Println("Word not found!")
+		return
+	}
 	// Print pronunciation
 	if entry.Basic.PhoneticUS != "" && entry.Basic.PhoneticUK != "" {
-		fmt.Println("美：[" + entry.Basic.PhoneticUS + "] | 英：[" + entry.Basic.PhoneticUK + "]")
+		fmt.Println(entry.Query+" |", color.MagentaString("美："), color.YellowString("["+entry.Basic.PhoneticUS+"]"),
+			"|", color.MagentaString("英："), color.YellowString("["+entry.Basic.PhoneticUK+"]"))
 	} else {
-		fmt.Println("[" + entry.Basic.Phonetic + "]")
+		// fmt.Println(entry.Query+" |", "["+entry.Basic.Phonetic+"]", getWordPinyin(entry.Basic.Phonetic))
+		printPronColor(entry.Basic.Phonetic)
 	}
 	fmt.Println()
 	// Print "explains"
-	fmt.Println("简明：")
+	color.Magenta("简明：")
 	for i, s := range entry.Basic.Explains {
 		var o string
 		if len(entry.Basic.Explains) <= 1 {
 			o = ""
 		} else {
-			o = strconv.Itoa(i+1) + ". "
+			o = color.GreenString(strconv.Itoa(i+1) + ". ")
 		}
 		fmt.Println(o + s)
 	}
-	fmt.Println()
+
 	// Print "web"
-	fmt.Println("短语：")
-	for i, web := range entry.Web {
-		o := strconv.Itoa(i + 1)
-		fmt.Println(o + ". " + web.Key)
-		for j, s := range web.Value {
-			if j == len(web.Value)-1 {
-				fmt.Println(s)
-			} else {
-				fmt.Print(s + ", ")
+	if entry.Web != nil {
+		fmt.Println()
+		color.Magenta("短语：")
+		for i, web := range entry.Web {
+			o := strconv.Itoa(i + 1)
+			fmt.Println(color.GreenString(o+"."), color.YellowString(web.Key))
+			for j, s := range web.Value {
+				if j == len(web.Value)-1 {
+					fmt.Println(s)
+				} else {
+					fmt.Print(s + ", ")
+				}
 			}
 		}
+	}
+	fmt.Println()
+}
+
+func printPronColor(pron string) {
+	firstTone := color.New(color.FgBlue).PrintfFunc()
+	secondTone := color.New(color.FgGreen).PrintfFunc()
+	thirdTone := color.New(color.FgYellow).PrintfFunc()
+	fourthTone := color.New(color.FgRed).PrintfFunc()
+	fifthTone := color.New(color.FgHiBlack).PrintfFunc()
+
+	wordsArr := getPinyinWords(pron)
+	if wordsArr == nil {
+		fmt.Println("["+pron+"]")
+	} else {
+		for _, s := range wordsArr {
+			tone := getWordPinyin(s)
+			switch(tone) {
+			case 1: firstTone(s)
+			case 2: secondTone(s)
+			case 3: thirdTone(s)
+			case 4: fourthTone(s)
+			case 5: fifthTone(s)
+			}
+		}
+	}
+
+}
+
+func getWordPinyin(pron string) int {
+	var tone int
+
+	out:
+	for i, t := range tones {
+		for _, s := range t {
+			if strings.Contains(pron, s) {
+				tone = i + 1
+				break out
+			}
+		}
+	}
+	return tone
+}
+
+func isPinyin(pron string) bool {
+	for i:=0;i<4;i++ {
+		for _, s := range tones[i] {
+			if strings.Contains(pron, s) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func getPinyinWords(pron string) []string {
+	if isPinyin(pron) {
+		if strings.Contains(pron, " ") {
+			return strings.Split(pron, " ")
+		} else {
+			return []string{pron}
+		}
+	} else {
+		return nil
 	}
 }
 
